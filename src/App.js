228ee2783b667348b51add7221f0f23e20156c6c
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-03-25 23:35:57 +0600

    data source added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [laptops, setLaptops] = useState([]);
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
   return (
     <div className="App">
       <h1>Hello world</h1>
